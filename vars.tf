a3c17aa59cf9581e6c214dc0c862f8a5108c1440
pavan <balubadi.pavankumar@gmail.com>
2024-06-30 16:44:32 +0530

    expense-terraform

--- vars.tf
+++ vars.tf
@@ -1,10 +1,9 @@
-variable "vpc" {}
 variable "env" {}
 variable "tags" {}
-variable "default_vpc_cidr" {}
-variable "default_vpc_id" {}
+variable "vpc_cidr_block" {}
+variable "public_subnets" {}
+variable "db_subnets" {}
+variable "web_subnets" {}
+variable "azs" {}
 variable "account_id" {}
 variable "default_route_table_id" {}
-variable "route53_zone_id" {}
-variable "bastion_cidrs" {}
-variable "prometheus_cidrs" {}
